d978093d6e120f6ecaefe997160b0223ffc6380f
TheDoritoboy <doritoboy.ekk@gmail.com>
2023-08-14 18:30:33 -0700

    Doing stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import React, { useState } from 'react'
-
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import './App.css'
+import Homepage from './Homepage/Homepage'
+import Header from './Header/Header'
+import Footer from './Footer/Footer'
 
 function App() {
 
   return (
-    <>
-  
-    </>
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+          <Route path='/' element =''/>
+      </Routes>
+    <Footer/>
+
+    </BrowserRouter>
   )
 }
 
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import { BsHeart } from "react-icons/bs";
+import { AiOutlineMail } from "react-icons/ai";
 function Header() {
   return (
-    <div>Header</div>
+    <div className='Header'>
+        {/*<img src="..\assets\holiday_village_FILL0_wght300_GRAD0_opsz48 3.png" alt="" />
+        <img src="..\assets\UniLife.svg" alt="" />*/}
+        <p><BsHeart/> Shortlist</p>
+        <p><AiOutlineMail/>Contact Us</p>
+    </div>
   )
 }
 
