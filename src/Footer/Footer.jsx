ec3947ccbb66ad4da984772559b73181fe0bf753
TheDoritoboy <doritoboy.ekk@gmail.com>
2023-08-21 19:54:34 -0700

    figuring out why my css isn't working

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <p>About Us</p>
+      <p>Terms & Conditions</p>
+      <p>Privacy & Cookie Policies</p>
+      <p> 2023 </p>
+      <img src="../assets/Vector.SVG" alt="" />
+      <p>UniLife</p>
+    </div>
   )
 }
 
